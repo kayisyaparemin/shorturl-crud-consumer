710f64fbbfa8cdc0ef7ff195a301ed0ce8536231
emin.kayisyapar <emin.kayisyapar@hangikredi.com>
2025-03-21 10:51:49 +0300

    getting connection string dynamic

--- data/PgDbContext.go
+++ data/PgDbContext.go
@@ -3,13 +3,45 @@ package dataContext
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 func ConnectDB() (*gorm.DB,error) {
-	dsn := "host=localhost user=admin password=password dbname=userdb port=5432 sslmode=disable"
+	dsn := ""
+	err := GetJSON("url",&dsn)
+	if err != nil {
+		return nil,err
+	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil,err
 	}
 	return db,nil
+}
+
+func GetJSON[T any](url string, result *T) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("request Fail: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error Code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("response was not readed: %w", err)
+	}
+
+	err = json.Unmarshal(body, result)
+	if err != nil {
+		return fmt.Errorf("json parse error: %w", err)
+	}
+
+	return nil
 }
